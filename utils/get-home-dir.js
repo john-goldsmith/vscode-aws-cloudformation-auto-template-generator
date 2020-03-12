623981e866b353f4f808fd27f22498ab7332a3cf
John Goldsmith <johngoldsm1@gmail.com>
2020-03-12 16:16:12 -0700

    docs: add docs for 'getHomeDir' util

--- utils/get-home-dir.js
+++ utils/get-home-dir.js
@@ -5,7 +5,10 @@
 const { homedir } = require('os')
 const { sep } = require('path')
 
-// See https://github.com/aws/aws-toolkit-vscode/blob/master/src/shared/credentials/credentialsFile.ts#L147-L162
+/**
+ * @return {String}
+ * @see https://github.com/aws/aws-toolkit-vscode/blob/master/src/shared/credentials/credentialsFile.ts#L147-L162
+ */
 function getHomeDir() {
   const { HOME, USERPROFILE, HOMEPATH, HOMEDRIVE = `C:${sep}` } = process.env
 
