f0531591833669dda31a942dc8cc71dc51e4d706
John Goldsmith <johngoldsm1@gmail.com>
2020-03-16 15:50:18 -0700

    test: improve 'insertResource' command tests

--- __mocks__/vscode.js
+++ __mocks__/vscode.js
@@ -1,24 +1,22 @@
 /**
- * @see https://code.visualstudio.com/api/references/vscode-api#commands.registerCommand
+ * @see https://code.visualstudio.com/api/references/vscode-api#commands
  */
 const commands = {
-  executeCommand: jest.fn().mockImplementation(() => Promise.resolve()),
-  registerCommand: jest.fn().mockImplementation(() => 1)
+  executeCommand: jest.fn(() => Promise.resolve()),
+  registerCommand: jest.fn(() => 1)
 }
 
+/**
+ * @see https://code.visualstudio.com/api/references/vscode-api#workspace
+ */
 const workspace = {
   getConfiguration: jest.fn()
-  // getConfiguration: jest.fn(() => {
-  //   return {
-  //     update: jest.fn()
-  //   }
-  // })
 }
 
 /**
  * @see https://code.visualstudio.com/api/references/vscode-api#Progress
  */
-const progress = {
+const Progress = {
   report: jest.fn()
 }
 
@@ -31,22 +29,7 @@ const window = {
   setStatusBarMessage: jest.fn(),
   showQuickPick: jest.fn(),
   showInputBox: jest.fn()
-  // withProgress: jest.fn().mockImplementation((options, cb) => {
-  //   cb(progress)
-  // })
 }
-
-/**
- * @see https://code.visualstudio.com/api/references/vscode-api#env
- */
-const env = {
-  openExternal: jest.fn()
-}
-
-/**
- * @see https://code.visualstudio.com/api/references/vscode-api#Uri
- */
-const Uri = jest.fn()
 
 /**
  * @see https://code.visualstudio.com/api/references/vscode-api#ProgressLocation
@@ -55,12 +38,29 @@ const ProgressLocation = {
   Window: 1
 }
 
+/**
+ * @see https://code.visualstudio.com/api/references/vscode-api#TextEditorEdit
+ *
+const TextEditorEdit = {
+  Window: 1
+}*/
+
+/**
+ * @see https://code.visualstudio.com/api/references/vscode-api#Position
+ */
+const Position = jest.fn()
+
+/**
+ * @see https://code.visualstudio.com/api/references/vscode-api#Selection
+ */
+const Selection = jest.fn()
+
 module.exports = {
   commands,
   window,
   workspace,
-  Uri,
-  env,
   ProgressLocation,
-  progress
+  Progress,
+  Position,
+  Selection
 }
